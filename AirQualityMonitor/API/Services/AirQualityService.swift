ae12dafebf3f5136cf7ac3add9450946d7fe21ce
Karol Piątek <kpiatek40@gmail.com>
2018-03-22 16:14:21 +0100

    Added bitrise status to readme

--- AirQualityMonitor/API/Services/AirQualityService.swift
+++ AirQualityMonitor/API/Services/AirQualityService.swift
@@ -52,8 +52,6 @@ extension AirQualityService: TargetType {
             return Data("{\"key\":\"NO2\",\"values\":[{\"date\":\"2018-03-20 11:00:00\",\"value\":28.7326},{\"date\":\"2018-03-20 10:00:00\",\"value\":34.7591},{\"date\":\"2018-03-20 09:00:00\",\"value\":42.5064}]}".utf8)
         case .stationData:
             return Data("{\"id\":114,\"stCalcDate\":\"2018-03-20 11:19:56\",\"stIndexLevel\":{\"id\":-1,\"indexLevelName\":\"Brak indeksu\"},\"stSourceDataDate\":\"2018-03-20 11:00:00\",\"so2CalcDate\":null,\"so2IndexLevel\":null,\"so2SourceDataDate\":null,\"no2CalcDate\":1521541196000,\"no2IndexLevel\":{\"id\":0,\"indexLevelName\":\"Bardzo dobry\"},\"no2SourceDataDate\":\"2018-03-20 11:00:00\",\"coCalcDate\":null,\"coIndexLevel\":null,\"coSourceDataDate\":null,\"pm10CalcDate\":null,\"pm10IndexLevel\":null,\"pm10SourceDataDate\":null,\"pm25CalcDate\":null,\"pm25IndexLevel\":null,\"pm25SourceDataDate\":null,\"o3CalcDate\":\"2018-03-20 11:19:56\",\"o3IndexLevel\":{\"id\":1,\"indexLevelName\":\"Dobry\"},\"o3SourceDataDate\":\"2018-03-20 11:00:00\",\"c6h6CalcDate\":null,\"c6h6IndexLevel\":null,\"c6h6SourceDataDate\":null,\"stIndexStatus\":false,\"stIndexCrParam\":\"PYL\"}".utf8)
-        default:
-            return Data("default value".utf8)
         }
     }
     
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AirQualityMonitor
 
 Used technologies: CocoaPods, Moya, RxSwift, corelocation.
+
+### Master branch build status:
+
+![](https://www.bitrise.io/app/05502995f0f6c050/status.svg?token=RrUHAoro50vkW24yVlE1sw&branch=master)
